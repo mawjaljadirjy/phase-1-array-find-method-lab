dcd3226df8750d0ac1e9dbe0dd3445768952353c
Mawj_Aljadirjy <eng_mawj_aljadirjy@yahoo.com>
2024-03-11 21:35:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    
+    const win = record.find(game => game.result === "W");
+      return win ? win.year : undefined;
+  }
+
+
+  
+  
